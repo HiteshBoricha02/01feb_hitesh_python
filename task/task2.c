b288a3fa9f7972813068f09ddef0547024ea7b9e
hiten-29 <124235599+hiten-29@users.noreply.github.com>
2023-02-21 14:01:29 +0530

    Add files via upload

---
+++ task/task2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+printf("faculty - nidhi nadiyara");
+printf("\nstudent \b's details");
+printf("\nstuden's name \tcourse name   \tage \tgender \tfees");
+printf("\nhitesh    \tBACKend-python\t21\tmale\t60,000");
+printf("\nmihir     \tFULLstack-java\t21\tmale\t90,000");
+printf("\nfaiz      \tMERNstack-nodeJS\b\t45\tother\t90,000 \vharshil    \tFULLstack-java\t25\tmale\t90,000");
+printf("\neshvar    \tfrontend-HTML\b\t21\tfemale\t90,000 \vRAJ         \tFULLstack-c\b\t25\tMALE\t90,000");
+printf("\nHITESH\r");
+printf("'          \\THANK you Ma\edam'/");
+}
