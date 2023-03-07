631e0927fe4782fcd32fb9e3dbfcf8cef7ea001a
HiteshBoricha02 <124235599+HiteshBoricha02@users.noreply.github.com>
2023-03-07 11:33:19 +0530

    Add files via upload

---
+++ practice/ifelseodd-even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter the even value:");
+    scanf("%d",&a);
+    if(a % 2 == 0)
+    {
+        printf("\n no is even.");
+
+    }
+    else
+    {
+        printf("\n no is odd.");
+    }
+
+    
+}
