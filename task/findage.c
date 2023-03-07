3ca8d5c5e1a2498e39e0dc8103a23fcbdb5a124e
HiteshBoricha02 <124235599+HiteshBoricha02@users.noreply.github.com>
2023-03-07 11:02:52 +0530

    Add files via upload

---
+++ task/findage.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+ int a;
+ int age;
+ printf("inter your age as on year 2023 : ");
+ scanf("%d",&age);
+ age--;
+ printf("your age in 2022 was : %d\n",age);
+ age++;
+ age++;
+ printf("your age in 2024 will be : %d\n",age);
+ 
+
+}
---
+++ task/task3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    int no=100;
+    //printf("plase enter the value");
+    //scanf("%d",&no);
+    printf("value is %d",no);
+}  
