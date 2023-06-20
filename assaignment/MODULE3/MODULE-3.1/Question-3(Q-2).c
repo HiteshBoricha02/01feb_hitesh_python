9be2703cddaa02e273b34f9bd6f43f8e4d5ca60b
HiteshBoricha02 <124235599+HiteshBoricha02@users.noreply.github.com>
2023-06-20 10:37:40 +0530

    Add files via upload

---
+++ assaignment/MODULE3/MODULE-3.1/Question-3(Q-2).c
@@ -0,0 +1,15 @@
+// find area of rectangle
+#include<stdio.h>
+void main()
+{
+    float area;
+    float width;
+    float length;
+ printf("enter a width of rectangle:");
+ scanf("%f",&width);
+ printf("\nenter a length of rectangle:");
+ scanf("%f",&length);
+ area = width*length;
+ printf("\n total area of rectangle is : %f",area);
+
+}
